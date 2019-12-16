d8fa9d99ae179d1e207a096afc33c4c2c1b96c2b
Anand Agrawal <45493325+anandagr21@users.noreply.github.com>
2019-12-16 16:02:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const flash = require("connect-flash");
+const session = require("express-session");
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 
