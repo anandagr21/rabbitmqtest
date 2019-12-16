5fc6d8d4cd61ae7fbd03c294a3837f4eaaad3aed
Anand Agrawal <45493325+anandagr21@users.noreply.github.com>
2019-12-16 15:00:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const publishToQueue = require('../rabbitmqm/services/MQService');
 
 router.post('/msg',async(req, res, next)=>{
     let { queueName, payload } = req.body;
-    await publishToQueue(queueName, payload);
+    await publishToQueue.publishToQueue(queueName, payload);
     res.statusCode = 200;
     res.data = {"message-sent":true};
     next();
